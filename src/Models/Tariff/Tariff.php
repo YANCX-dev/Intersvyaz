d3055c967b30cd98b86a1ffc3e0a0c8ceb82985d
Yancx-dev <ybabuskin@gmail.com>
2024-12-25 19:10:27 +0300

    Добавил сохранение в csv, загрузку из csv, поправил редактирование картинки, и другие мелкие исправления

--- src/Models/Tariff/Tariff.php
+++ src/Models/Tariff/Tariff.php
@@ -44,31 +44,48 @@ class Tariff extends BaseModel
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    /**
-     * @param $id
-     * @param array $data
-     * @return bool
-     */
+
     public function updateTariffById($id, array $data): bool
     {
         $queryPath = __DIR__ . '/sqls/updateTariffById.sql';
 
-        $query = SqlHelper::getSqlQuery($queryPath);
+        try {
+            $query = SqlHelper::getSqlQuery($queryPath);
 
-        $stmt = $this->pdo->prepare($query);
 
+            if (empty($data['logo'])) {
+                $query = preg_replace('/,\s*logo\s*=\s*:logo/', '', $query);
+            }
 
-        return $stmt->execute([
-            'id' => $id,
-            'name' => $data['name'],
-            'description' => $data['description'],
-            'price' => $data['price'],
-            'expires_at' => $data['expires_at'],
-            'created_at' => $data['created_at'],
-            'speed' => $data['speed'],
-            'logo' => $data['logo'] ?? null,
-        ]);
+            $stmt = $this->pdo->prepare($query);
+
+            $params = [
+                'id' => $id,
+                'name' => $data['name'],
+                'description' => $data['description'],
+                'price' => $data['price'],
+                'expires_at' => $data['expires_at'],
+                'created_at' => $data['created_at'],
+                'speed' => $data['speed'],
+            ];
+
+            if (!array_key_exists('logo', $data)) {
+                return $stmt->execute($params);
+            }
+
+            $params['logo'] = $data['logo'];
+
+            return $stmt->execute($params);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
+    private function dd(mixed $args)
+    {
+        echo '<pre>';
+        var_dump($args);
+        echo '</pre>';
+    }
 
 }
